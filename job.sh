a71a594c585e523e1846f6731b0596527712f328
Phineas Pham <72993615+Ph1n-Pham@users.noreply.github.com>
2024-04-15 08:17:13 -0400

    add command examples

---
+++ job.sh
@@ -0,0 +1,13 @@
+python prompt.py --model_path openlm-research/open_llama_3b_v2 --tokenizer_path openlm-research/open_llama_3b_v2 
+python prompt2.py --model_path openlm-research/open_llama_7b_v2 --tokenizer_path openlm-research/open_llama_7b_v2
+python prompt.py --model_path openlm-research/open_llama_13b --tokenizer_path openlm-research/open_llama_13b 
+
+python prompt.py --model_path allenai/OLMo-1B --tokenizer_path allenai/OLMo-1B
+python prompt.py --model_path allenai/OLMo-7B --tokenizer_path allenai/OLMo-7B
+python prompt.py --model_path allenai/OLMo-7B-Twin-2T --tokenizer_path allenai/OLMo-7B-Twin-2T
+
+python prompt.py --model_path lmsys/vicuna-7b-v1.5 --tokenizer_path lmsys/vicuna-7b-v1.5
+python prompt.py --model_path lmsys/vicuna-13b-v1.5 --tokenizer_path lmsys/vicuna-13b-v1.5
+
+python prompt.py --model_path tiiuae/falcon-7b --tokenizer_path tiiuae/falcon-7b
+python prompt.py --model_path tiiuae/falcon-40b --tokenizer_path tiiuae/falcon-40b
